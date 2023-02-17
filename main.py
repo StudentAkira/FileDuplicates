e195385b15c83af2b910582c8c0e6c201f4a8abd
Akira <am.1.00@bk.ru>
2023-02-17 23:34:18 +0300

    Using bytes
    File Bytes as dict keys - much faster. Path input emplemented;Copyies are founded;

--- main.py
+++ main.py
@@ -11,23 +11,26 @@ start = time.time()
 
 def foo(folder_path):
 
-    folder_normalized_path = ''.join([x if x != '\\' else x+'\\'  for x in folder_path])
+    folder_normalized_path = ''.join([x if x != '\\' else x+x  for x in folder_path])
 
     for root, dirs, files in os.walk(folder_normalized_path):
 
         for file in files:
             try:
                 with open(root+'\\'+file,'rb')as f:
-                    hs = hashlib.md5(f.read()).hexdigest()
+                    hs = f.read()
                     if not file_hashes.get(hs):
                         file_hashes[hs] = 1
                         continue
+                    file_hashes[hs] += 1
                     repeated_files_pathes.append(root + '\\' + file)
             except:
                 hs = 'ERROR'
     print(repeated_files_pathes)
 
-
 foo(input('enter_folder_path :: '))
+
+
+
 
 print(time.time() - start)
