b62c77f2a95cb0893161cb36bd9e74e55b17e942
Akira <am.1.00@bk.ru>
2023-02-24 11:54:43 +0300

    ClientServer
    Client - Server dialog established. Asynchrony inmplemented.

--- clie1.py
+++ clie1.py
@@ -2,4 +2,17 @@ import socket
 import time
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('192.168.100.25', 5555))
+client_socket.connect(('localhost', 5555))
+
+while True:
+    test = input()
+    client_socket.send(test.encode())
+    print(client_socket.recv(1024))
+
+'''
+print(client_socket.recv(1024))
+while True:
+    data = input(' :: ')
+    client_socket.send(data.encode())
+    print(client_socket.recv(1024))
+'''
--- server.py
+++ server.py
@@ -1,53 +1,34 @@
 import socket
 from select import select
-import time
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', 5555))
 server_socket.listen()
 
 
-def server():
-    while True:
-        print('Waiting for connection (blocking):: ')
-        yield server_socket
-        client_socket, addr = server_socket.accept() # read
-        client_socket.send(b'You have been connected')
-        to_read[client_socket] = client(client_socket)
-
-
-def client(client_socket):
-    while True:
-        print('Receiving data (blocking):: ')
-        yield client_socket
-        try:
-            received_data = client_socket.recv(1024) # read
-            print(received_data)
-            print('Sending data (blocking):: ')
-            yield client_socket
-        except:
-            del to_read[client_socket]
-            continue
-        try:
-            client_socket.send(f'Time is ::{str(time.time())}'.encode()) # write
-        except ConnectionResetError:
-            del to_read[client_socket]
-
-
-
 def event_loop():
-    next(to_read[server_socket])
 
     while True:
-        ready_to_read, _, _ = select(list(to_read), [], [])
+        ready_to_read, ready_to_write, _ = select(list(to_read), list(to_write), [])
+
+        for sock in ready_to_write:
+            sock.send(b'somedata')
+            del to_write[sock]
 
         for sock in ready_to_read:
-            next(to_read[sock])
+            if sock == server_socket:
+                client_socket, addr = server_socket.accept()  # read
+                print(addr, 'connected')
+                to_read[client_socket] = client_socket
+                continue
+            print(sock.recv(1024))
+            to_write[sock] = sock
 
 
 to_read = {
-    server_socket: server()
+    server_socket: server_socket
 }
+to_write = {}
 
 event_loop()
 
